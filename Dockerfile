8bda591386dbe6d9a235428d563712444517b133
PICCIRILLI <simone.a.piccirilli@capgemini.com>
2023-01-24 15:37:40 +0100

    client fix e docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,36 +1,15 @@
-FROM openjdk:17-jdk-slim-buster as builder
+FROM openjdk:17-jdk-alpine
 
-# Set the working directory to /app
+# Set the working directory
 WORKDIR /app
 
-LABEL name="SMOC0"
+# Copy the JAR file
+COPY target/SMOC0-0.0.1-SNAPSHOT.jar /app/IWDB0.jar
 
-# Copy the pom.xml file to the container
-COPY pom.xml .
-
-# Build all the dependencies in preparation to go offline. 
-# This is a separate step so the dependencies will be cached unless the pom.xml file has changed.
-RUN mvn dependency:go-offline -B
-
-# Copy the project source code to the container
-COPY . .
-
-# Build the application
-RUN mvn package
-
-FROM openjdk:17-jre-slim-buster
-
-# Set the working directory to /app
-WORKDIR /app
-
-# Copy the built jar file from the builder image
-COPY --from=builder /app/target/*.jar app.jar
-
-# Set the environment variable
-ENV JAVA_OPTS="-Xmx256m -Xms256m"
-
+# Expose the port
 EXPOSE 8089
 
-# Run the jar file
-ENTRYPOINT ["java", "-Djava.security.egd=file:/dev/./urandom","-jar", "app.jar"]
+LABEL name="smoc-img"
 
+# Run the application
+CMD ["java", "-jar", "IWDB0.jar"]
--- src/main/java/com/saga/orchestrator/service/fragment/Iwdb0Client.java
+++ src/main/java/com/saga/orchestrator/service/fragment/Iwdb0Client.java
@@ -1,10 +1,10 @@
 package com.saga.orchestrator.service.fragment;
 
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.saga.orchestrator.model.request.DispoRequest;
 import com.saga.orchestrator.model.request.ProdottiRequest;
@@ -16,8 +16,10 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class Iwdb0Client {
-
-	private String iwdbUri = "http://localhost:8083";
+	
+	@Value("${config.iwdb0.end-point}")
+	private String iwdbUri;
+	
 	WebClient webClient = WebClient.create(iwdbUri);
 
 	// transaction Prodotto
@@ -26,9 +28,10 @@ public class Iwdb0Client {
 		ProdottiResponse iResp = new ProdottiResponse();
 		Mono<ProdottiResponse> response = null;
 		
+		String uri = UriComponentsBuilder.fromHttpUrl(iwdbUri + "prodotti/transact").toUriString();
 		try {
 			response = webClient.post()
-					.uri("prodotti/transact")
+					.uri(uri)
 					.accept(MediaType.APPLICATION_JSON)
 					.contentType(MediaType.APPLICATION_JSON)
 					.body(Mono.just(request), ProdottiRequest.class)
@@ -55,9 +58,10 @@ public class Iwdb0Client {
 		ProdottiResponse iResp = new ProdottiResponse();
 		Mono<ProdottiResponse> response = null;
 
+		String uri = UriComponentsBuilder.fromHttpUrl(iwdbUri + "prodotti/rollb").toUriString();
 		try {
 			response = webClient.post()
-					.uri("prodotti/rollb")
+					.uri(uri)
 					.accept(MediaType.APPLICATION_JSON)
 					.contentType(MediaType.APPLICATION_JSON)
 					.body(Mono.just(request), ProdottiRequest.class)
@@ -78,10 +82,11 @@ public class Iwdb0Client {
 
 		DispoResponse iResp = new DispoResponse();
 		Mono<DispoResponse> response = null;
-
+		
+		String uri = UriComponentsBuilder.fromHttpUrl(iwdbUri + "/dispo/transaction").toUriString();
 		try {
 			response = webClient.post()
-					.uri("/dispo/transaction")
+					.uri(uri)
 					.accept(MediaType.APPLICATION_JSON)
 					.contentType(MediaType.APPLICATION_JSON)
 					.body(Mono.just(request), ProdottiRequest.class)
@@ -122,8 +127,10 @@ public class Iwdb0Client {
 		request.setTrxId(trxId);
 		request.setStatus(status);
 		
+		String uri = UriComponentsBuilder.fromHttpUrl(iwdbUri + "/ordini/update/"+trxId+"/"+status).toUriString();
+		
 		return webClient.put()
-				.uri("/ordini/update/"+trxId+"/"+status)
+				.uri(uri)
 				.accept(MediaType.APPLICATION_JSON)
 				.contentType(MediaType.APPLICATION_JSON)
 				.retrieve()
